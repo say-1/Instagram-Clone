49490026dc32339e6a1a90e0ae382a9c5326246a
Ritika <04.ritikaa@gmail.com>
2025-07-17 21:33:44 +0530

    url changed

--- backend/index.js
+++ backend/index.js
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 });
 app.use(urlencoded({ extended: true }));
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: process.env.URL,
   credentials: true,
 };
 app.use(cors(corsOptions));
